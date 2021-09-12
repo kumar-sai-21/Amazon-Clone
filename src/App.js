90d71c197cb838ccba05f087bb265789b7efebbd
Sai kumar Satapathy <saikumar212000@gmail.com>
2021-09-12 23:51:24 +0530

    app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import './App.css';
-import Header from './Header';
-import Home from './Home';
+import React from "react";
+import "./App.css";
+import Header from "./Header"
+import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 function App() {
-  return (
+return (
 
+  <Router>
     <div className="App">
+      <Switch>
     
-    <Header/>
-    <Home/>
-    </div> 
-  );
+        <Route path="/">
+          <Header/>
+          <Home />
+        </Route>
+      </Switch>
+    </div>
+  </Router>
+);
 }
 
 export default App;
