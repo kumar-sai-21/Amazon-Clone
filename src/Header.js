a1e0976381437a5d2a49a5622662af9510d2502d
Asutosh-panda <pandasutosh@gmail.com>
2021-09-15 14:37:56 +0530

    Responsive Ui with hamburger menu

--- src/Header.js
+++ src/Header.js
@@ -1,18 +1,35 @@
-import React from 'react'
+import React,{useState} from 'react'
 import "./Header.css"
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 
 function Header() {
+    const [toggle,setToggle] = useState(false)
+
     const myFunction=()=>{
         
+        if(toggle){
+            console.log(toggle,"close")
+            document.querySelector('.header_mobile').style.opacity = "0";
+            document.querySelector('.header_mobile').style.height = "0vh";
+
+        }
+        else{
+            document.querySelector('.header_mobile').style.opacity = "1";
+            document.querySelector('.header_mobile').style.height = "30vh";
+            console.log(toggle,"open")
+        }
+        setToggle(!toggle)
     }
+
     return (
+        <div className="nav_head">
         <div className = "header">
         <img 
         className="header__logo"
         src ="https://zeevector.com/wp-content/uploads/LOGO/Amazon-India-Logo-PNG-White2.png"
         alt =""/>
+            
         <div className="header_options">
         <div className= "header__search">
         <input
@@ -21,6 +38,7 @@ function Header() {
         {/*logo*/}
         <SearchIcon className="header__searchIcon"/>
         </div>
+    
 
         <div className="header__nav">
             <div className='header_option'>
@@ -31,7 +49,7 @@ function Header() {
         </div>
 
         <div className='header_option'>
-        <div className='header_option'/>
+       
             <span className='header_optionLineOne'>
             Return </span>
             <span
@@ -40,7 +58,7 @@ function Header() {
         </div>
 
         <div className='header_option'>
-        <div className='header_option'/>
+       
             <span className='header_optionLineOne'>
             Yours</span>
             <span
@@ -48,19 +66,47 @@ function Header() {
 
         </div>
 
-            <div className="header_optionBasket">
+           
+
+        </div>
+        </div>
+
+        <div className="header_optionBasket">
                 <ShoppingBasketIcon/>
                 <span
                 className='header_optionLineTwo headerbasketCount'> 0 </span>
             </div>
-
-        </div>
-        </div>
        
         
-         <i  onclick="myFunction()" className="fa fa-bars"></i>
+         <i  onClick={myFunction} className="fa fa-bars hamburger" style={{fontSize:"2rem",marginRight:"1rem",color:"#e6880d"}}></i>
     
         </div>
+        
+        
+    <div className="header_mobile">
+        <div className="header__nav_mobile">
+            <div className='header_option_mobile'>
+            <span className='header_option_mobileLineOne'>
+            Hello Guest</span>
+            <span className='header_option_mobileLineOne'>
+            Return </span>
+            <span className='header_option_mobileLineOne'>
+            Your Orders </span>
+            <span className='header_option_mobileLineOne'>
+            Prime </span>
+            </div>      
+        </div>
+        <div className= "header__search">
+        <input
+        className="header__searchInput"
+        type="text"/>
+        {/*logo*/}
+        <SearchIcon className="header__searchIcon"/>
+        </div>
+        </div>
+     
+        </div>
+        
     )
 }
 
